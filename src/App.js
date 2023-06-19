89acff02ab18c04ef59aeabb55880819ba65d98d
Katri Pokats <katri.pokats@gmail.com>
2023-06-19 07:03:56 +0300

    Remove default react logo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import VacationForm from "./components/VacationForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <VacationForm/>
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <VacationForm/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
