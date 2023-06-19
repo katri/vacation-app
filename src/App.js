efd663d97a407e53539fb8493d5ba64baf96e414
Katri Pokats <katri.pokats@gmail.com>
2023-06-19 07:18:33 +0300

    Render VacationList on App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import VacationForm from "./components/VacationForm";
+import VacationList from "./components/VacationList";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <VacationForm/>
+
+                <VacationList/>
             </header>
         </div>
     );
