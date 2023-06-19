f3df8e2cb6ab6402280b628a184b3f34e395224a
Katri Pokats <katri.pokats@gmail.com>
2023-06-19 07:26:38 +0300

    Add data to be rendered as VacationList

--- src/components/VacationList.js
+++ src/components/VacationList.js
@@ -1,39 +1,34 @@
 import React, {useEffect, useState} from 'react';
+import vacations from '../data/vacations';
 
 const VacationList = () => {
-    const [vacationRequests, setVacationRequests] = useState([]);
-
-    useEffect(() => {
-        fetchVacationRequests();
-    }, []);
-
-    const fetchVacationRequests = async () => {
-        try {
-            // todo Replace API endpoint with  actual API endpoint
-            const response = await fetch('/api/vacation-requests');
-            const data = await response.json();
-            setVacationRequests(data);
-        } catch (error) {
-            console.log('Error fetching vacation requests:', error);
-        }
-    };
 
     return (
         <div>
-            <h2>My Vacation Requests</h2>
-            {vacationRequests.length === 0 ? (
-                <p>No vacation requests found.</p>
-            ) : (
-                <ul>
-                    {vacationRequests.map((request) => (
-                        <li key={request.id}>
-                            <div>Start Date: {request.startDate}</div>
-                            <div>End Date: {request.endDate}</div>
-                            <div>Number of Days: {request.numDays}</div>
-                            <div>Comment: {request.comment}</div>
-                        </li>
+            <h2>Vacation Requests</h2>
+            {vacations.length > 0 ? (
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th>Number of Vacation Days</th>
+                        <th>Comment</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {vacations.map((vacation) => (
+                        <tr key={vacation.id}>
+                            <td>{vacation.startDate}</td>
+                            <td>{vacation.endDate}</td>
+                            <td>{vacation.numDays}</td>
+                            <td>{vacation.comment}</td>
+                        </tr>
                     ))}
-                </ul>
+                    </tbody>
+                </table>
+            ) : (
+                <p>No vacation requests found.</p>
             )}
         </div>
     );
---
+++ src/data/vacations.js
@@ -0,0 +1,18 @@
+const vacations = [
+    {
+        id: 1,
+        startDate: '2023-06-01',
+        numDays: 7,
+        endDate: '2023-06-07',
+        comment: 'Family trip',
+    },
+    {
+        id: 2,
+        startDate: '2023-07-15',
+        numDays: 10,
+        endDate: '2023-07-24',
+        comment: 'Beach vacation',
+    },
+];
+
+export default vacations;
